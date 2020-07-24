975e977f96ec10c552020110ea3d07c4b467f20a
Brooke Storm <bstorm@wikimedia.org>
2020-07-24 11:08:15 -0700

    correct things in the setup.py for this repo

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="nbpawspublic",
-    version='0.1.0',
-    url="https://github.com/yuvipanda/nbpawspublic",
+    version='0.1.1',
+    url="https://github.com/toolforge/nbpawspublic",
     author="Yuvi Panda",
     packages=setuptools.find_packages(),
     install_requires=[
