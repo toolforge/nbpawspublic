e1c21fdaa482a26c6ba44a7d0105335d63a65373
Brooke Storm <bstorm@wikimedia.org>
2020-07-24 09:56:45 -0700

    the rest of the initial commit

---
+++ nbpawspublic/__init__.py
@@ -0,0 +1,8 @@
+def _jupyter_nbextension_paths():
+    return [{
+        "section": "notebook",
+        "dest": "nbpawspublic",
+        "src": "static",
+        "require": "nbpawspublic/main"
+    }]
+
